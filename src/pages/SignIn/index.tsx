4b19593091eb978567e7f12207572bd82572ff16
AndreCosta101 <andre.costa101@gmail.com>
2020-10-17 19:17:32 -0300

    validação de login

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,37 +1,72 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
 
+import { FormHandles } from '@unform/core'
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErrors';
+
 import logoImg from '../../assets/logo.svg';
+
 
 import Input from '../../components/Input'
 import Button from '../../components/Button'
 
 import { Container, Content, Background } from './styles';
 
-const SignIn: React.FC = () => (
-  <Container>
-    <Content>
-      <img src={logoImg} alt="GoBarber" />
+const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null)
 
-      <form>
-        <h1>Faça seu logon</h1>
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      //zerar os erros, para a mensagem de erro sumir ao gravar pela segunda vez.
+      formRef.current?.setErrors({})
 
-        <Input name="email" icon={FiMail} placeholder="E-mail" />
+      const schema = Yup.object().shape({
+        email: Yup.string().required('Email obrigatório').email('Digite um email válido'),
+        password: Yup.string().required('Senha obrigatória'),
+      })
 
-        <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
+      /*
+        * o Yup tem como padrão retornar apenas o ultimo erro que encontra.
+        * o abortEarly: false faz ele retornar todos os erros.
+      */
+      await schema.validate(data, {
+        abortEarly: false
+      });
 
-        <Button type="submit">Entrar</Button>
+    } catch (err) {
+      const errors = getValidationErrors(err)
 
-        <a href="forgot">Esqueci minha senha</a>
-      </form>
+      formRef.current?.setErrors(errors)
+    }
+  }, [])
 
-      <a href="login">
-        <FiLogIn />
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Faça seu logon</h1>
+
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+
+          <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
+
+          <Button type="submit">Entrar</Button>
+
+          <a href="forgot">Esqueci minha senha</a>
+        </Form>
+
+        <a href="login">
+          <FiLogIn />
         Criar Conta
       </a>
-    </Content>
-    <Background />
-  </Container>
-)
+      </Content>
+      <Background />
+    </Container >
+  )
+}
 
 export default SignIn;
