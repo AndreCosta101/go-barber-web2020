1cd90a6b060718dfd6d5bedee92e053cd979d672
AndreCosta101 <andre.costa101@gmail.com>
2020-10-18 16:08:33 -0300

    Logout da aplicação

--- src/hooks/AuthContext.tsx
+++ src/hooks/AuthContext.tsx
@@ -14,6 +14,7 @@ interface SignInCredentials {
 interface AuthContextData {
   user: object;
   signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void;
 }
 
 //para poder iniciar um objeto vazio, as AuthContext
@@ -46,8 +47,15 @@ const AuthProvider: React.FC = ({ children }) => {
     setData({ token, user })
   }, [])
 
+  const signOut = useCallback(() => {
+    const token = localStorage.removeItem('@GoBarber:token');
+    const user = localStorage.removeItem('@GoBarber:user');
+
+    setData({} as AuthState)
+  }, [])
+
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   )
